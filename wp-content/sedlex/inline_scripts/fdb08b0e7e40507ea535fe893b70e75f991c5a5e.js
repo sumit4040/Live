8c6232b30f2a93a823b7bda663372065be39d897
root <root@server.serverhunk.com>
2017-02-05 11:55:53 -0500

    first commit

---
+++ wp-content/sedlex/inline_scripts/fdb08b0e7e40507ea535fe893b70e75f991c5a5e.js
@@ -0,0 +1,23 @@
+
+var gdpData = {
+};
+				jQuery(function(){
+					jQuery('#geolocate_show_world').vectorMap({
+						map: 'world_mill_en',
+						backgroundColor: '#A1A1A1',
+						series: {
+						    regions: [{
+						      values: gdpData,
+							  hoverOpacity: 0.7,
+    						  hoverColor: false,
+						      scale: ['#C8EEFF', '#000066'],
+						      normalizeFunction: 'polynomial'
+						    }]
+						},
+						onRegionLabelShow: function(e, el, code){
+						    el.html(el.html()+' ('+gdpData[code]+')');
+						}
+					});
+				}) ; 
+				
+			
